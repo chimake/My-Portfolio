96357f7044b6d4ea223010e37c723f58dc1bfebb
chimake <dev.chimaobi@gmail.com>
2023-01-30 18:00:13 +0100

    Fixing lint errors

--- script.js
+++ script.js
@@ -5,29 +5,21 @@ const menuContainer = document.querySelector('.menu-container');
 const mobileLinKItem = document.querySelectorAll('.mobile-link-item');
 
 ham.addEventListener('click', () => {
-    for (let i = 0; i < sections.length; i += 1) {
-        sections[i].classList.remove('blur');
-    }
-
-    menuContainer.style.display = 'block';
-    
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].classList.remove('blur');
+  }
+  menuContainer.style.display = 'block';
 });
 
 closeMenu.addEventListener('click', () => {
-    for (let i = 0; i < sections.length; i += 1) {
-        sections[i].classList.remove('blur');
-    }
-
-    menuContainer.style.display = 'none';
-    
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].classList.remove('blur');
+  }
+  menuContainer.style.display = 'none';
 });
 
 for (let i = 0; i < mobileLinKItem.length; i += 1) {
-    mobileLinKItem[i].addEventListener('click', () => {
-        // for (let i = 0; i < sections.length; i += 1) {
-        //     sections[i].classList.remove('blur');
-        // }
-
-        menuContainer.style.display = 'none';
-    });
+  mobileLinKItem[i].addEventListener('click', () => {
+    menuContainer.style.display = 'none';
+  });
 }
