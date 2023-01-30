644fc5518c361ab6474779c24d10931a161c35df
isaacg <iwanyoike65@gmail.com>
2023-01-30 19:06:30 +0300

    Add javascript for the mobile menu pop up

---
+++ script.js
@@ -0,0 +1,33 @@
+const ham = document.querySelector('.menu-hamburger-trigger');
+const closeMenu = document.querySelector('.close');
+const sections = document.querySelectorAll('section');
+const menuContainer = document.querySelector('.menu-container');
+const mobileLinKItem = document.querySelectorAll('.mobile-link-item');
+
+ham.addEventListener('click', () => {
+    for (let i = 0; i < sections.length; i += 1) {
+        sections[i].classList.remove('blur');
+    }
+
+    menuContainer.style.display = 'block';
+    
+});
+
+closeMenu.addEventListener('click', () => {
+    for (let i = 0; i < sections.length; i += 1) {
+        sections[i].classList.remove('blur');
+    }
+
+    menuContainer.style.display = 'none';
+    
+});
+
+for (let i = 0; i < mobileLinKItem.length; i += 1) {
+    mobileLinKItem[i].addEventListener('click', () => {
+        // for (let i = 0; i < sections.length; i += 1) {
+        //     sections[i].classList.remove('blur');
+        // }
+
+        menuContainer.style.display = 'none';
+    });
+}
