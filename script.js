32eaa72d634fa926c12581fec883dd34a0943d2a
chimake <dev.chimaobi@gmail.com>
2023-02-02 20:14:56 +0100

    Fixing lint errors

--- script.js
+++ script.js
@@ -26,67 +26,67 @@ for (let i = 0; i < mobileLinKItem.length; i += 1) {
 }
 
 const project = [{
-  name: "Tonic",
-  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  longdescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  imagesource: "assets/images/SnapshootPortfolio.png",
-  altmessage: "project1",
+  name: 'Tonic',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  imagesource: 'assets/images/SnapshootPortfolio.png',
+  altmessage: 'project1',
   technologies: ['html', 'css', 'javascript'],
   projectsummary: ['CANOPY', 'Back end dev', '2015'],
   livelink: ['#', 'See live', 'assets/images/seelive.png'],
   sourcelink: ['#', 'See Source', 'assets/images/seesource.png'],
-  buttontext: "See Project",
-  uniqueid: "0",
-  cancel: "assets/images/portfoliocancel.png"
+  buttontext: 'See Project',
+  uniqueid: '0',
+  cancel: 'assets/images/portfoliocancel.png',
 
 },
 {
-  name: "Multi-Post Stories",
-  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  longdescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  imagesource: "assets/images/SnapshootPortfolio2.png",
-  altmessage: "project2",
+  name: 'Multi-Post Stories',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  imagesource: 'assets/images/SnapshootPortfolio2.png',
+  altmessage: 'project2',
   technologies: ['html', 'css', 'javascript'],
   projectsummary: ['CANOPY', 'Back end dev', '2015'],
   livelink: ['#', 'See live', 'img/seelive.png'],
   sourcelink: ['#', 'See Source', 'img/seesource.png'],
-  buttontext: "See Project",
-  uniqueid: "1",
-  cancel: "assets/images/portfoliocancel.png"
+  buttontext: 'See Project',
+  uniqueid: '1',
+  cancel: 'assets/images/portfoliocancel.png',
+
 },
 {
-  name: "Tonic",
-  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  longdescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  imagesource: "assets/images/GymfitSnapshootPortfolio.png",
-  altmessage: "project3",
+  name: 'Tonic',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  imagesource: 'assets/images/GymfitSnapshootPortfolio.png',
+  altmessage: 'project3',
   technologies: ['html', 'css', 'javascript'],
   projectsummary: ['CANOPY', 'Back end dev', '2015'],
   livelink: ['#', 'See live', 'assets/images/seelive.png'],
   sourcelink: ['#', 'See Source', 'assets/images/seesource.png'],
-  buttontext: "See Project",
-  uniqueid: "2",
-  cancel: "assets/images/portfoliocancel.png"
-
+  buttontext: 'See Project',
+  uniqueid: '2',
+  cancel: 'assets/images/portfoliocancel.png',
 
 },
 {
-  name: "Multi-Post Stories",
-  description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  longdescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  imagesource: "assets/images/ProfessionalSnapshootPortfolio.png",
-  altmessage: "project4",
+  name: 'Multi-Post Stories',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  imagesource: 'assets/images/ProfessionalSnapshootPortfolio.png',
+  altmessage: 'project4',
   technologies: ['html', 'css', 'javascript'],
   projectsummary: ['CANOPY', 'Back end dev', '2015'],
   livelink: ['#', 'See live', 'assets/images/seelive.png'],
   sourcelink: ['#', 'See Source', 'assets/images/seesource.png'],
-  buttontext: "See Project",
-  uniqueid: "3",
-  cancel: "assets/images/portfoliocancel.png"
+  buttontext: 'See Project',
+  uniqueid: '3',
+  cancel: 'assets/images/portfoliocancel.png',
 
+},
+];
 
-}
-]
 const header = document.querySelector('#works');
 const about = document.querySelector('#about-me-more');
 const contact = document.querySelector('#contact-me');
@@ -96,7 +96,6 @@ const portfoliopopup = document.querySelector('.portfolio-popup');
 function generateHtml(tag, tagClassName) {
   tag = document.createElement(tag);
   tag.className = tagClassName;
-  
   return tag;
 }
 
@@ -106,33 +105,32 @@ function addTextAppend(tag, text, parent) {
   return tag;
 }
 
-function fetchPortfolio() {
+window.addEventListener('load', () => {
   for (let i = 0; i < project.length; i += 1) {
-    //let div = generateHtml('div', 'con con1 con-1');
-    let article = generateHtml('article', 'media');
-    let mediaDivClasses = ["media-img img-1","media-img img-2","media-img img-3","media-img img-4"];
-    let mediaImg = generateHtml('div',mediaDivClasses[i]);
-    let mediaImgTag = new Image();
+    const article = generateHtml('article', 'media');
+    const mediaDivClasses = ['media-img img-1', 'media-img img-2', 'media-img img-3', 'media-img img-4'];
+    const mediaImg = generateHtml('div', mediaDivClasses[i]);
+    const mediaImgTag = new Image();
     // Add image
     mediaImgTag.src = project[i].imagesource;
     mediaImgTag.alt = project[i].altmessage;
-    let divMediaBody = generateHtml('div','media-body');
-    let portfolioHeader = generateHtml('h4');
-    let divRoleSummary = generateHtml('div','role');
-    let span1 = generateHtml('span');
-    let spanDot1 = generateHtml('i');
-    let span2 = generateHtml('span');
-    let spanDot2 = generateHtml('i');
-    let span3 = generateHtml('span');
-    let divDescription = generateHtml('div','description');
-    let paragraphDescription = generateHtml('p');
-    let ulSkills = generateHtml('ul','skills');
-    let liSkills1 = generateHtml('li');
-    let liSkills2 = generateHtml('li');
-    let liSkills3 = generateHtml('li');
-    let divReadmore = generateHtml('div','read-more');
-    let readMeMoreAnchor = document.createElement('a');
-    readMeMoreAnchor.href = "#";
+    const divMediaBody = generateHtml('div', 'media-body');
+    const portfolioHeader = generateHtml('h4');
+    const divRoleSummary = generateHtml('div', 'role');
+    const span1 = generateHtml('span');
+    const spanDot1 = generateHtml('i');
+    const span2 = generateHtml('span');
+    const spanDot2 = generateHtml('i');
+    const span3 = generateHtml('span');
+    const divDescription = generateHtml('div', 'description');
+    const paragraphDescription = generateHtml('p');
+    const ulSkills = generateHtml('ul', 'skills');
+    const liSkills1 = generateHtml('li');
+    const liSkills2 = generateHtml('li');
+    const liSkills3 = generateHtml('li');
+    const divReadmore = generateHtml('div', 'read-more');
+    const readMeMoreAnchor = document.createElement('a');
+    readMeMoreAnchor.href = '#';
 
     portfolio.appendChild(article);
     article.appendChild(mediaImg);
@@ -140,7 +138,7 @@ function fetchPortfolio() {
     article.appendChild(divMediaBody);
     addTextAppend(portfolioHeader, project[i].name, divMediaBody);
     divMediaBody.appendChild(divRoleSummary);
-    addTextAppend(span1, project[i].projectsummary[0],divRoleSummary);
+    addTextAppend(span1, project[i].projectsummary[0], divRoleSummary);
     divRoleSummary.appendChild(spanDot1);
     addTextAppend(span2, project[i].projectsummary[1], divRoleSummary);
     divRoleSummary.appendChild(spanDot2);
@@ -153,18 +151,13 @@ function fetchPortfolio() {
     addTextAppend(liSkills3, project[i].technologies[2], ulSkills);
     divMediaBody.appendChild(divReadmore);
     addTextAppend(readMeMoreAnchor, project[i].buttontext, divReadmore);
+  }
 
-
-
-
-
-  } //end of for loop
-
-  let buttons = document.querySelectorAll('.read-more a');
-  let toolbar = document.querySelector('.toolbar');
+  const buttons = document.querySelectorAll('.read-more a');
+  const toolbar = document.querySelector('.toolbar');
 
   for (let i = 0; i < buttons.length; i += 1) {
-    buttons[i].addEventListener('click', function () {
+    buttons[i].addEventListener('click', () => {
       header.classList.add('blur');
       toolbar.style.display = 'none';
       about.style.display = 'none';
@@ -173,56 +166,48 @@ function fetchPortfolio() {
       portfoliopopup.style.display = 'flex';
       window.scrollTo(0, 0);
 
-      let popupdiv = generateHtml('div', 'popup-div');
-
-      let divtitle = document.createElement('div');
-      divtitle.className = "div-title";
-      let divtop = generateHtml('div', 'div-top');
-
-      let divbottom = generateHtml('div', 'div-bottom');
-      let divbutton = generateHtml('div', 'div-button');
-      let popupimage = new Image();
-      let divtitlename = document.createElement('h2');
-      let divtitlecanopy = generateHtml('p', 'canopy');
-      let divtitlebackend = generateHtml('p', 'backend');
-      let divtitleyear = generateHtml('p', 'y15');
-      let divtitlecounter1 = generateHtml('i','dot');
-      let divtitlecounter2 = generateHtml('i','dot');
-      let divtitlecancel = new Image();
-      let frametitle = generateHtml('div', 'frame-title');
-      let framecanopy = generateHtml('div', 'frame-canopy');
-      let frameLanguage = generateHtml('div', 'frame-language');
+      const popupdiv = generateHtml('div', 'popup-div');
+      const divtitle = document.createElement('div');
+      divtitle.className = 'div-title';
+      const divtop = generateHtml('div', 'div-top');
+      const divbottom = generateHtml('div', 'div-bottom');
+      const divbutton = generateHtml('div', 'div-button');
+      const popupimage = new Image();
+      const divtitlename = document.createElement('h2');
+      const divtitlecanopy = generateHtml('p', 'canopy');
+      const divtitlebackend = generateHtml('p', 'backend');
+      const divtitleyear = generateHtml('p', 'y15');
+      const divtitlecounter1 = generateHtml('i', 'dot');
+      const divtitlecounter2 = generateHtml('i', 'dot');
+      const divtitlecancel = new Image();
+      const frametitle = generateHtml('div', 'frame-title');
+      const framecanopy = generateHtml('div', 'frame-canopy');
+      const frameLanguage = generateHtml('div', 'frame-language');
 
       addTextAppend(divtitlename, project[i].name, frametitle);
       divtitle.appendChild(frametitle);
 
       divtitlecancel.src = project[i].cancel;
-      divtitlecancel.className = "portfolio1cancel";
+      divtitlecancel.className = 'portfolio1cancel';
       frametitle.appendChild(divtitlecancel);
       divtitle.appendChild(frametitle);
 
-
-      addTextAppend(divtitlecanopy, project[i].projectsummary[0], framecanopy)
-
+      addTextAppend(divtitlecanopy, project[i].projectsummary[0], framecanopy);
 
       divtitle.appendChild(framecanopy);
 
-     
       framecanopy.appendChild(divtitlecounter1);
       divtitle.appendChild(framecanopy);
-
 
       addTextAppend(divtitlebackend, project[i].projectsummary[1], framecanopy);
       divtitle.appendChild(framecanopy);
 
-      
       framecanopy.appendChild(divtitlecounter2);
       divtitle.appendChild(framecanopy);
 
       addTextAppend(divtitleyear, project[i].projectsummary[2], framecanopy);
-      divtitleyear.className = "y15";
+      divtitleyear.className = 'y15';
       divtitle.appendChild(framecanopy);
-
 
       portfoliopopup.appendChild(popupdiv);
       popupdiv.appendChild(divtitle);
@@ -231,28 +216,24 @@ function fetchPortfolio() {
       divtop.appendChild(popupimage);
       popupimage.src = project[i].imagesource;
       popupimage.alt = project[i].altmessage;
-      popupimage.className = "popup-image";
+      popupimage.className = 'popup-image';
 
-      let divbottomdescription = generateHtml('p', 'popup-description');
-      addTextAppend(divbottomdescription, project[i].longdescription, divbottom)
+      const divbottomdescription = generateHtml('p', 'popup-description');
+      addTextAppend(divbottomdescription, project[i].longdescription, divbottom);
 
-
-      let divbottomlanguage = generateHtml('ul', 'bottom-list');
+      const divbottomlanguage = generateHtml('ul', 'bottom-list');
       for (let x = 0; x < project[i].technologies.length; x += 1) {
-        let divbottomlanguageListitem = document.createElement('li');
-        let cs = "modal-list";
-        if (project[i].technologies[x] === "html") {
+        const divbottomlanguageListitem = document.createElement('li');
+        const cs = 'modal-list';
+        if (project[i].technologies[x] === 'html') {
           divbottomlanguageListitem.textContent = project[i].technologies[x];
           divbottomlanguageListitem.className = cs;
-
-        } else if (project[i].technologies[x] === "css") {
+        } else if (project[i].technologies[x] === 'css') {
           divbottomlanguageListitem.textContent = project[i].technologies[x];
           divbottomlanguageListitem.className = cs;
-
-        } else if (project[i].technologies[x] === "javascript") {
+        } else if (project[i].technologies[x] === 'javascript') {
           divbottomlanguageListitem.textContent = project[i].technologies[x];
           divbottomlanguageListitem.className = cs;
-
         }
 
         divbottomlanguage.appendChild(divbottomlanguageListitem);
@@ -261,27 +242,33 @@ function fetchPortfolio() {
       frameLanguage.appendChild(divbottomlanguage);
       divbottom.appendChild(frameLanguage);
 
-      let divbottombutton1 = generateHtml('a', 'projectbutton');
-      divbottombutton1.type = "button";
-      divbottombutton1.href = project[i].livelink[0];
-      let image1 = new Image();
-      image1.src = project[i].livelink[2];
-      addTextAppend(divbottombutton1, project[i].livelink[1], divbutton);
+      const divbottombutton1 = generateHtml('a', 'projectbutton');
+      divbottombutton1.type = 'button';
+      const liveLink0 = project[i].livelink[0];
+      const liveLink1 = project[i].livelink[1];
+      const liveLink2 = project[i].livelink[2];
+
+      divbottombutton1.href = liveLink0;
+      const image1 = new Image();
+      image1.src = liveLink2;
+      addTextAppend(divbottombutton1, liveLink1, divbutton);
       divbottombutton1.appendChild(image1);
       frameLanguage.appendChild(divbutton);
       divbottom.appendChild(frameLanguage);
 
-      let divbottombutton2 = generateHtml('a', 'projectbutton');
-      divbottombutton2.type = "button";
-      divbottombutton2.href = project[i].sourcelink[0];
-      let image2 = new Image();
-      image2.src = project[i].sourcelink[2];
+      const divbottombutton2 = generateHtml('a', 'projectbutton');
+      divbottombutton2.type = 'button';
+      const sourcelink1 = project[i].sourcelink[0];
+      divbottombutton2.href = sourcelink1;
+      const image2 = new Image();
+      const sourceLink2 = project[i].sourcelink[2];
+      image2.src = sourceLink2;
       addTextAppend(divbottombutton2, project[i].sourcelink[1], divbutton);
       divbottombutton2.appendChild(image2);
       frameLanguage.appendChild(divbutton);
       divbottom.appendChild(frameLanguage);
 
-      let c = document.querySelector('.portfolio1cancel');
+      const c = document.querySelector('.portfolio1cancel');
       c.addEventListener('click', () => {
         header.classList.remove('blur');
         toolbar.style.display = 'block';
@@ -291,8 +278,6 @@ function fetchPortfolio() {
         portfolio.style.display = 'grid';
         popupdiv.remove();
       });
-
     });
-
   }
-}
+});
